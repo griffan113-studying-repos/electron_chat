5c9d6549f29960d2d49bd6332e36e2116bb89823
Eliel M. Gaspar <elielmichel@gmail.com>
2021-06-30 20:57:41 -0300

    First Commit

---
+++ electron/main.ts
@@ -0,0 +1,51 @@
+import { app, BrowserWindow, ipcMain } from 'electron';
+
+let mainWindow: BrowserWindow | null
+
+declare const MAIN_WINDOW_WEBPACK_ENTRY: string
+declare const MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY: string
+
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    minHeight: 600,
+    minWidth: 500,
+    width: 1100,
+    height: 700,
+    backgroundColor: '#FFF',
+    webPreferences: {
+      nodeIntegration: false,
+      worldSafeExecuteJavaScript: true,
+      contextIsolation: true,
+      preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY
+    }
+  })
+
+  mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY)
+
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+}
+
+async function registerListeners () {
+  ipcMain.on('message', (_, message) => {
+    console.log(message)
+  })
+}
+
+app.on('ready', createWindow)
+  .whenReady()
+  .then(registerListeners)
+  .catch(e => console.error(e))
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
