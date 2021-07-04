6696c2e8e7ec548bb2b5bad8b661882ff9e2c9b9
Eliel M. Gaspar <elielmichel@gmail.com>
2021-07-04 16:08:50 -0300

    Layout 1

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import ReactDOM from "react-dom";
 import { App } from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('chatApp'));
--- webpack/rules.webpack.js
+++ webpack/rules.webpack.js
@@ -1,23 +1,26 @@
-module.exports = [
-  {
-    test: /\.node$/,
-    use: 'node-loader',
-  },
-  {
-    test: /\.(m?js|node)$/,
-    parser: { amd: false },
-    use: {
-      loader: '@marshallofsound/webpack-asset-relocator-loader',
-      options: {
-        outputAssetBase: 'native_modules',
-      },
+module.exports = [{
+  test: /\.node$/,
+  use: 'node-loader',
+},
+{
+  test: /\.(m?js|node)$/,
+  parser: { amd: false },
+  use: {
+    loader: '@marshallofsound/webpack-asset-relocator-loader',
+    options: {
+      outputAssetBase: 'native_modules',
     },
   },
-  {
-    test: /\.(js|ts|tsx)$/,
-    exclude: /node_modules/,
-    use: {
-      loader: 'babel-loader'
-    }
+},
+{
+  test: /\.(js|ts|tsx)$/,
+  exclude: /node_modules/,
+  use: {
+    loader: 'babel-loader'
   }
+},
+{
+  test: /\.css$/i,
+  use: ["style-loader", "css-loader"],
+},
 ]
